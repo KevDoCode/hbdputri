067553a39f3326600d02fd9435e9a97074911512
KevDoCode <62963736+KevDoCode@users.noreply.github.com>
2020-04-26 20:23:11 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("27 April 2020 00:00:00 GMT+0500");			
+		var endTime = new Date("27 April 2020 00:00:00 GMT+0900");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
